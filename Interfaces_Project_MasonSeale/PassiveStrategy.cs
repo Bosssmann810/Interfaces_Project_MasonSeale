d545eb85666ab1df24f224b5384a6c5daf6a7ced
Bosssmann810 <sealered@gmail.com>
2026-01-28 11:12:37 -0400

    added a passivestrategy where the enemy runs away from the player

---
+++ Interfaces_Project_MasonSeale/PassiveStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Project_MasonSeale
+{
+    internal class PassiveStrategy
+    {
+        public Position Move(Position position, Player player)
+        {
+            if (position._x > player._playerpos._x)
+            {
+                position._x += 1;
+            }
+            if (position._x < player._playerpos._x)
+            {
+                position._x -= 1;
+            }
+            if (position._y > player._playerpos._y)
+            {
+                position._y += 1;
+            }
+            if (position._y < player._playerpos._y)
+            {
+                position._y -= 1;
+            }
+
+            return position;
+        }
+
+    }
+}
